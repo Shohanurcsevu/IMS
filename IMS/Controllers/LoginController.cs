b530b25dab08e887a53ddc298a623f6bc4e1cf7b
Shohanur Rahman <shohanurvu@gmail.com>
2019-02-06 11:16:19 +0600

    Initial commit

---
+++ IMS/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using IMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(IMS.Models.User userModel)
+        {
+            using (IMSEntities db = new IMSEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if(userDetails==null)
+                {
+                    userModel.LoginErrorMessage = "Wrong Username or Password";
+                    return View("Index", userModel);
+                }
+
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+                    Session["UserName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+
+               
+
+            }
+        }
+
+         public ActionResult LogOut()
+        {
+            int userId = (int)Session["UserID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+} 
